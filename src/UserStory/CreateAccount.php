75e5e31b866194f19773ea565caa3039b668d0b4
adrienb39 <adrienb39@yahoo.com>
2024-10-16 09:16:23 +0200

    tech: add pseudo, email and password in execute and test

--- src/UserStory/CreateAccount.php
+++ src/UserStory/CreateAccount.php
@@ -22,18 +22,34 @@ class CreateAccount
     public function execute(string $pseudo, string $email, string $password): User {
         // Vérifier que les données sont présentes
         // Si tel n'est pas le cas, lancer une exception
+        if (empty($pseudo) || empty($email) || empty($password)) {
+            throw new \Exception("Tous les champs sont obligatoires");
+        }
 
         // Vérifier si l'email est valide
         // Si tel n'est pas le cas, lancer une exception
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \Exception("Email invalide");
+        }
 
         // Vérifier si le pseudo est entre 2 et 50 caractères
         // Si tel n'est pas le cas, lancer une exception
+        if (strlen($pseudo) < 2 || strlen($pseudo) > 50) {
+            throw new \Exception("Le pseudo doit contenir entre 2 et 50 caractères");
+        }
 
         // Vérifier si le mot de passe est sécurisé
         // Si tel n'est pas le cas, lancer une exception
+        if (strlen($password) < 8 || !preg_match('/[A-Z]/', $password) || !preg_match('/[a-z]/', $password) || !preg_match('/[0-9]/', $password)) {
+            throw new \Exception("Le mot de passe doit contenir au moins 8 caractères, avec des majuscules, des minuscules et des chiffres");
+        }
 
         // Vérifier l'unicité de l'email
         // Si tel n'est pas le cas, lancer une exception
+        $existEmail = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+        if ($existEmail) {
+            throw new \Exception('Email déjà existant');
+        }
 
         // Insérer les données dans la base de données
         // 1. Hash le mot de passe
--- tests/create-account-test.php
+++ tests/create-account-test.php
@@ -1,2 +1,14 @@
 <?php
 
+use App\Entity\User;
+use App\UserStory\CreateAccount;
+
+/**
+ * @var Doctrine\ORM\EntityManager $entityManager
+ */
+$entityManager = require_once __DIR__.'/../config/bootstrap.php';
+require_once __DIR__.'/../vendor/autoload.php';
+
+$user = new CreateAccount($entityManager);
+$user->execute("test", "oezmshfmeh564@hzeiuhf.com", "jtroigmdfgjhioerlmhsg554848A");
+$user = $entityManager->getRepository(User::class)->findAll();
